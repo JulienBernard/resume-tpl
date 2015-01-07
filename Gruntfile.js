8a6828795b8d8a8bead9a5495ad5a12817ecade4
JulienBernard <jbernard@intechinfo.fr>
2015-01-07 23:40:20 +0100

    Configure Grunt tasks. Basic HTML starter.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,53 @@ module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 	
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded',
+                    sourcemap: 'none'
+                },
+                files: {
+                    './build/app.css': './src/scss/app.scss'
+                }
+            }
+        },
+
+        autoprefixer: {
+            dist: {
+                src: './build/app.css',
+                dest: './build/app.css'
+            }
+        },
+
+        copy: {
+            dist: {
+                src: './src/index-dev.html',
+                dest: './build/index.html'
+            }
+        },
+
+        watch: {
+            styles: {
+                files: ["./src/scss/*.scss"],
+                tasks: ["make-styles"]
+            },
+            index: {
+                files: ["./src/index.html"],
+                tasks: ["copy:dist"]
+            }
+        },
+
+        clean: {
+            build: {
+                src: ["./build/*"]
+            }
+        }
     });
-	
-    grunt.registerTask('default', []);
+
+    grunt.registerTask('default', ['clean:build', 'copy:dist', 'make-styles']);
+    grunt.registerTask("make-styles", ["sass:dist", "autoprefixer:dist"])
+    grunt.registerTask('clean-build', ['clean:build']);
 };
