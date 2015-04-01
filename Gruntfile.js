f915390367827f968af5b10eb6e873e687b103d8
JulienBernard <jbernard@intechinfo.fr>
2015-04-01 21:27:25 +0200

    Add cssmin in order to minify the css files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,36 +11,54 @@ module.exports = function(grunt) {
                     sourcemap: 'none'
                 },
                 files: {
-                    './build/app.css': './src/scss/app.scss',
-                    './build/print.app.css': './src/scss/print.app.scss'
+                    './build/tmp/app.css': './src/scss/app.scss',
+                    './build/tmp/print.css': './src/scss/print.app.scss'
                 }
             }
         },
 
         autoprefixer: {
             dist: {
-                src: './build/app.css',
-                dest: './build/app.css'
+                src: './build/tmp/app.css',
+                dest: './build/tmp/app.css'
             }
         },
 		
 		tinypng: {
-			options: {
-			  apiKey: "YOUR API KEY HERE",
-			  showProgress: true
-			},
-			assets: {
-			  expand: true,
-			  cwd: 'src/assets/',
-			  src: 'images/*.*',
-			  dest: 'build/'
-			},
+		  options: {
+			apiKey: "YOUR API KEY HERE",
+			showProgress: true
 		  },
+		  assets: {
+			expand: true,
+			cwd: 'src/assets/',
+			src: 'images/*.*',
+			dest: 'build/'
+		  },
+		},
+		
+		cssmin: {
+		  minify: {
+			files: [{
+			  expand: true,
+			  cwd: 'build/tmp/',
+			  src: ['*.css', '!*.min.css'],
+			  dest: 'build/',
+			  ext: '.min.css'
+			}]
+		  }
+		},
 
         copy: {
             html: {
                 src: './src/index-dev.html',
                 dest: './build/index.html'
+            },
+			fonts: {
+                expand: true,
+                cwd: './src/assets/fonts/',
+                src: '*.*',
+                dest: './build/fonts/'
             },
             libs: {
                 expand: true,
@@ -53,26 +71,25 @@ module.exports = function(grunt) {
         watch: {
             styles: {
                 files: ["./src/scss/*.scss"],
-                tasks: ["make-styles"]
+                tasks: ["make-styles", "cssmin:minify", "clean:tmp"]
             },
             index: {
                 files: ["./src/index-dev.html"],
                 tasks: ["copy:html"]
-            },
-            assets: {
-                files: ["./src/assets/*"],
-                tasks: ["copy:assets"]
             }
         },
 
         clean: {
             build: {
-                src: ["./build/*"]
+                src: ["./build"]
+            },
+			tmp: {
+                src: ["./build/tmp"]
             }
         }
     });
 
-    grunt.registerTask('default', ['clean:build', 'copy:html', 'tinypng:assets', 'copy:libs', 'make-styles']);
-    grunt.registerTask("make-styles", ["sass:dist", "autoprefixer:dist"])
+    grunt.registerTask('default', ['clean:build', 'copy:html', 'copy:libs', 'copy:fonts', 'tinypng:assets', 'make-styles', 'cssmin:minify', 'clean:tmp']);
+    grunt.registerTask('make-styles', ["sass:dist", "autoprefixer:dist"])
     grunt.registerTask('clean-build', ['clean:build']);
 };
